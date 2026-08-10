42fbc701c072e7be395fa12b4c7d351787cecbdf
Tomingremeau <tomingremeaumaillet@gmail.com>
2026-08-10 13:46:58 +0200

    mise à jour des liens prod sans les /

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://kasa-e2qm.onrender.com/",
+  baseURL: "https://kasa-e2qm.onrender.com",
   //baseURL: "http://localhost:3001",
 });
 
